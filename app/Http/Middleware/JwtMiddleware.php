1d2c7dd11f302e9ca546ea96f441075d9d7a70d2
Rizal Haryaputra <rzlhryptr@gmail.com>
2026-03-25 14:38:04 +0700

    feat(auth): add JwtMiddleware and replace sanctum guard in routes

---
+++ app/Http/Middleware/JwtMiddleware.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Exception;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+
+class JwtMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+        } catch (Exception $e) {
+            if ($e instanceof TokenInvalidException) {
+                return response()->json(['meta' => ['message' => 'Token tidak valid', 'status' => 'error']], 401);
+            } else if ($e instanceof TokenExpiredException) {
+                return response()->json(['meta' => ['message' => 'Token sudah kedaluwarsa', 'status' => 'error']], 401);
+            } else {
+                return response()->json(['meta' => ['message' => 'Token otorisasi tidak ditemukan', 'status' => 'error']], 401);
+            }
+        }
+
+        return $next($request);
+    }
+}
